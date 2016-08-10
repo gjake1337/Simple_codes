3ad11c09c9cfe5090fc5b7fd3e3fd1c570deeb01
Jake Goodwin <jakegoodwin1337@gmail.com>
2016-08-10 10:32:10 -0700

    Update __init__.py
    Changed the way that the program was generating a key. It now uses an actual list instead of a string. The string was what caused some of the type errors before, I was like a fool attempting to index through the string using a integer. This revisions make_alpha function can edit a alphabet(saved as a list) by shifting over the location of all the characters in the array.
        Might redit the program so that when 0 is given as an argument for start_point it throws an error instead of just derping.

--- __init__.py
+++ __init__.py
@@ -1,33 +1,24 @@
 # -*- coding: utf-8 -*-
 #Jake Goodwin 2016
 #pre defined stuff
-alpha = 'abcdefghijklmnopqrstuvwyxz'
-alpha_applied = {'a': None, 'b': None, 'c': None, 'd': None, 'e': None, 
-'f': None, 'g': None, 'h': None, 'i': None, 'j': None, 'k': None, 'l': None,
-'m': None, 'n': None, 'o': None, 'p': None, 'q': None, 'r': None, 's': None,
-'t': None, 'u': None, 'v': None, 'w': None, 'y': None, 'x': None, 'z': None}
-print(alpha_applied['a'])
-userinput = input('Please enter your plain text:')
-#pull out the index of the value given for the alphabet
-def pullout(IV):
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+         'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'x', 'z']
+alpha_applied = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+         'm', 'n','o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'x', 'z']
+start_point = None
+#function to iterate through a loop
+def make_alpha(alphabet, alphabet_key, start_point):
     index = 0
-    for i in alpha:
-        if i == IV:
-           return index
-           break
-        else:
-            index = index + 1
-#applying the shift to the key/ dict data structure
-def shift_alphabet(alphabet_key, alphabet):
-    shift_index = input("please enter a shift:")
-    for i in alphabet_key:
-        print(i, shift_index)
-        alphabet_key[i] = alphabet[shift_index]
-        shift_index = shift_index + 1
-        if shift_index == len(alphabet) - 1:
-            shift_index = 0
-        elif alphabet_key[i] == alphabet[shift_index]:
-            print('function finished')
-            break
-shift_alphabet(alpha_applied, alpha)
+    start_index = start_point
+    Token = True
+    while Token:
+        alphabet_key[index] = alphabet[start_index]
+        index = index + 1
+        start_index = start_index + 1
+        if start_index == (len(alphabet) - 1):
+            start_index = 0
+        elif start_index == start_point:
+            Token = False
+print(alpha_applied)
+make_alpha(alpha, alpha_applied, 12)
 print(alpha_applied)
