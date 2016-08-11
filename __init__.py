b148532e83d066b9095d9c835eff5ba659cd6fb9
Jake Goodwin <jakegoodwin1337@gmail.com>
2016-08-11 12:01:04 -0700

    Update __init__.py
    Closer to completion, I'm still exploring the way that for loops in python iterate through the different data types. It seems that there are still some errors in this source code, but I think I should have those ironed out in the next revision.

--- __init__.py
+++ __init__.py
@@ -6,6 +6,8 @@ alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 alpha_applied = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
          'm', 'n','o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'x', 'z']
 start_point = None
+user_string = None
+user_list = []
 #function to iterate through a loop
 def make_alpha(alphabet, alphabet_key, start_point):
     index = 0
@@ -19,6 +21,22 @@ def make_alpha(alphabet, alphabet_key, start_point):
             start_index = 0
         elif start_index == start_point:
             Token = False
+#apply the cifer to the user's input
+def encrypt_string(alphabet, alphabet_key, user_input):
+    for index in range(len(user_input)):
+        for i in alphabet:
+            if user_input[index] == alphabet[i]:
+                user_input[index] = alphabet_key[i]
+                continue
+#converting a string into a list
+def string_to_list(string, user_list):
+    for i in string:
+        user_list.append(i)
+#main beef of the program, the heart if you will...
 print(alpha_applied)
-make_alpha(alpha, alpha_applied, 12)
+make_alpha(alpha, alpha_applied, int(input('Please enter a shift:')))
 print(alpha_applied)
+user_string = input('please enter your message:')
+string_to_list(user_string, user_list)
+print(user_list)
+encrypt_string(alpha, alpha_applied, user_list)
